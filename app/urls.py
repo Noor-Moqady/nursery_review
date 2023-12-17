5a60ec88cf352766b3d3c60dc6a0be0959a6b876
noor-moq <noor.moqady@gmail.com>
2023-12-17 12:22:14 +0200

    commit

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('', views.render_app)
+    path('', views.render_nursery),
+    path('about', views.render_aboutus),
+    path('register', views.render_register),
+    path('contact', views.render_contactus),
+    path('team', views.render_nurseries)
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
-def render_app(request):
+def render_nursery(request):
     return render (request, "index.html")
+
+def render_aboutus(request):
+    return render (request, "about.html")
+
+def render_register(request):
+    return render (request, "appointment.html")
+
+def render_contactus(request):
+    return render (request, "contact.html")
+
+
+def render_nurseries(request):
+    return render (request, "team.html")
