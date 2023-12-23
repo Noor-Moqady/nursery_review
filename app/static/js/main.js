d3020d6606326dc3d76a622835b571a256c57082
noor-moq <noor.moqady@gmail.com>
2023-12-23 18:11:42 +0200

    commit

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -24,8 +24,8 @@
             $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
         }
     });
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -35,7 +35,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
@@ -47,8 +47,8 @@
         items: 1,
         dots: true,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
         ]
@@ -62,23 +62,66 @@
         margin: 24,
         dots: false,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
         ],
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            992:{
-                items:2
+            992: {
+                items: 2
             }
         }
     });
-    
+
 
 
 
 })(jQuery);
+
+
+function openConfirmDialog(title, message) {
+
+    alertify.confirm(title, message,
+        function () {
+            // Action when confirmed
+        },
+        function () {
+            // Action when cancelled
+        }
+    ).set({
+        labels: { ok: 'OK', cancel: '' }, // Customize button labels if needed
+        defaultFocus: 'ok', // Set default focus to the 'OK' button
+        show: {
+            cancel: false // Hide the cancel button
+        },
+        reverseButtons: true
+    });
+
+    $(".alertify .ajs-footer .ajs-buttons .ajs-button.ajs-ok").css({
+        "background-color": "rgb(254, 93, 55)",
+        "border-color": "rgb(254, 93, 55)",
+        "border-radius": "50rem",
+        "width": "135px",
+        "height": "50px",
+        "color": "#fff"
+    });
+
+}
+
+function msg(event, element) {
+    event.preventDefault();
+
+    //work
+    openConfirmDialog('test1', 'test2 loooong message')
+    //alert("Appointment successfully made! we will contact you as soon as possible!");
+
+
+}
+
+
+
 
